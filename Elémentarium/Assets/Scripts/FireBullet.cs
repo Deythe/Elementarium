c188745237028fae305ee1421e30f9305995de30
Deythe <deythe123@gmail.com>
2022-10-14 13:00:17 +0200

    Animation mains + Jet d'eau

--- Elémentarium/Assets/Scripts/FireBullet.cs
+++ Elémentarium/Assets/Scripts/FireBullet.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class FireBullet : MonoBehaviour
 {
     [SerializeField] private InputActionProperty triggerAction, gripAction;
+    [SerializeField] private Transform spawnPoint; 
     [SerializeField] private GameObject bulletPrefabs;
     [SerializeField] private int force;
     private GameObject bullet;
@@ -22,7 +24,7 @@ public class FireBullet : MonoBehaviour
     public void ShootBullet()
     {
         bullet = Instantiate(bulletPrefabs);
-        bullet.transform.position = transform.position;
+        bullet.transform.position = spawnPoint.transform.position;
         bullet.GetComponent<Rigidbody>().AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
