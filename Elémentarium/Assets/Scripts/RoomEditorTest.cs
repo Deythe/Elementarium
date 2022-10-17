199f75773e10adeea7d9f3c5775ce6b4a2953f6d
Deythe <deythe123@gmail.com>
2022-10-17 08:47:23 +0200

    Premier jet editeur de room

--- Elémentarium/Assets/Scripts/RoomEditorTest.cs
+++ Elémentarium/Assets/Scripts/RoomEditorTest.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class RoomEditorTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int xSizeRoom, ySizeRoom, zSizeRoom;
+    [SerializeField] private GameObject prefab;
+    private GameObject transformParent, cubePivot;
+    private int x, y, z;
+    
+    [ContextMenu("Create Room Structure")]
+    void CreatureRoomStructure()
     {
+        if (xSizeRoom<4 || ySizeRoom<4 || zSizeRoom<4)
+        {
+            Debug.LogError("Une des 3 valeur rentrée est incorrect car inférieur à la limite minimum");   
+            return;
+        }
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    [ContextMenu("Test")]
-    void Test()
-    {
-        
+        transformParent = new GameObject();
+        transformParent.name = "Room";
+        for (x = 0; x < xSizeRoom; x++)
+        {
+            for (y = 0; y < ySizeRoom; y++)
+            {
+                for (z = 0; z < zSizeRoom; z++)
+                {
+                    if (y == 0 || y == ySizeRoom-1 || x == 0 || x == xSizeRoom-1 || z == 0 || z == zSizeRoom-1)
+                    {
+                        cubePivot = Instantiate(prefab, transformParent.transform);
+                        cubePivot.transform.position = new Vector3(x, y, z);   
+                    }
+                }
+            }
+        }
     }
 }
