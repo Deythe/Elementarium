1c8137f9ff8a8a3219276e00fb6ed7ac0b6a63b0
IssaGandega <issagndega94@gmail.com>
2022-10-23 15:37:25 +0200

    Modifs renderer

--- Elémentarium/Assets/Scripts/Expulse.cs
+++ Elémentarium/Assets/Scripts/Expulse.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using DG.Tweening;
 using UnityEngine.XR;
 
 
@@ -11,49 +12,26 @@ public class Expulse : MonoBehaviour
 {
     [SerializeField] private InputActionProperty triggerAction;
     [SerializeField] private Transform spawnPoint; 
-    [SerializeField] private GameObject bulletPrefabs;
-    [SerializeField] private int force;
-    [SerializeField] private float bloom;
+    [SerializeField] ParticleSystem inkParticle;
+    [SerializeField] Transform parentController;
+    [SerializeField] Transform splatGunNozzle;
 
-    [Range(0.01f, 1f)]
-    [SerializeField] private float cooldownMin;
-    [Range(0.01f, 1f)]
-    [SerializeField] private float cooldownMax;
-    private float cooldown;
-    private bool canFire = true;
-
-    private GameObject bullet;
-    private bool canShoot = true;
-
-    private void Start()
-    {
-        cooldown = UnityEngine.Random.Range(cooldownMin, cooldownMax);
-    }
+    private bool isShooting = false;
 
     public void Update()
     {
-        if (canFire && triggerAction.action.ReadValue<float>() > 0.1f)
+        Vector3 angle = parentController.localEulerAngles;
+        
+        if (triggerAction.action.ReadValue<float>() > 0.1f)
         {
-            ShootBullet();
+            inkParticle.Play();
+            isShooting = true;
+        }
+        else if (isShooting)
+        {
+            inkParticle.Stop();
+            isShooting = false;
         }
     }
-
-    public void ShootBullet()
-    {
-        bullet = Pooler.instance.Pop("Water");
-        bullet.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        bullet.transform.position = spawnPoint.transform.position;
-        bullet.GetComponent<Rigidbody>().AddForce((transform.forward + transform.right * UnityEngine.Random.Range(-bloom, bloom) + transform.up * UnityEngine.Random.Range(-bloom, bloom)).normalized * force, ForceMode.Impulse);
-        StartCoroutine(CooldownCoroutine(cooldown));
-    }
-
-    IEnumerator CooldownCoroutine(float t) 
-    {
-        canFire = false;
-
-        yield return new WaitForSeconds(t);
-
-        cooldown = UnityEngine.Random.Range(cooldownMin, cooldownMax);
-        canFire = true;
-    }
+    
 }
