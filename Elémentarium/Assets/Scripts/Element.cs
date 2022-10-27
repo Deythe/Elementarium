a6fa48dd1c5e0e6ea5bcf53701cd88cf001f8dbf
Xavier L <xavier.lezzoche@gmail.com>
2022-10-27 15:38:51 +0200

    Logique des containers (ajout/retrait de fluide, rotation pour vider)

--- Elémentarium/Assets/Scripts/Element.cs
+++ Elémentarium/Assets/Scripts/Element.cs
@@ -6,4 +6,11 @@ using UnityEngine;
 public class Element : ScriptableObject
 {
     public string elementName;
+    public int elementID;
+    public float mass; //mass for 1m^3
+
+    public float GetMass() 
+    {
+        return this.mass;
+    }
 }
