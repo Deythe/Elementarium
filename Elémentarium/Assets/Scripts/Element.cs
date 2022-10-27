eab40ef4b0ae95b529c9392059639f53fbfb2d7d
Xavier L <xavier.lezzoche@gmail.com>
2022-10-27 11:23:30 +0200

    Reset Door + tout début d'Elements

---
+++ Elémentarium/Assets/Scripts/Element.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Elements")]
+public class Element : ScriptableObject
+{
+    public string elementName;
+}
