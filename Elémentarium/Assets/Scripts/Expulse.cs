f68d1ca8d8927993ad3992734ae1b154d9b0e4ea
GANDEGA <i.gandega@rubika-edu.com>
2022-11-08 12:40:17 +0100

    fire + file cleanup

--- Elémentarium/Assets/Scripts/Expulse.cs
+++ Elémentarium/Assets/Scripts/Expulse.cs
@@ -11,45 +11,20 @@ using UnityEngine.XR;
 public class Expulse : MonoBehaviour
 {
     [SerializeField] private InputActionProperty triggerAction;
-    [SerializeField] ParticleSystem inkParticle;
-    [SerializeField] Transform parentController;
-    [SerializeField] Transform splatGunNozzle;
-    [SerializeField] [Range(0, 1)] private float cooldownMin, cooldownMax;
-
-    private float cooldown;
-    private GameObject bullet;
-
-    private void Start()
-    {
-        cooldown = UnityEngine.Random.Range(cooldownMin, cooldownMax);
-    }
+    [SerializeField] ParticleSystem elemParticle;
 
     public void Update()
     {
-        Vector3 angle = parentController.localEulerAngles;
-        
         if (triggerAction.action.WasPerformedThisFrame())
         {
-            VisualPolish();
-            inkParticle.Play();
+            elemParticle.gameObject.SetActive(true);
+            elemParticle.Play();
         }
         else if (triggerAction.action.WasReleasedThisFrame())
         {
-            inkParticle.Stop();
+            elemParticle.Stop();
+            elemParticle.gameObject.SetActive(false);
         }
     }
-    
-    void VisualPolish()
-    {
-        if (!DOTween.IsTweening(parentController))
-        {
-            parentController.DOComplete();
-            Vector3 forward = -parentController.forward;
-            Vector3 localPos = parentController.localPosition;
-            parentController.DOLocalMove(localPos - new Vector3(0, 0, .2f), .03f)
-                .OnComplete(() => parentController.DOLocalMove(localPos, .1f).SetEase(Ease.OutSine));
 
-        }
-    }
-    
 }
