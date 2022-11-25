f168a2a37a561fcb052feb07fb90e923002f9b27
GANDEGA <issagndega94@gmail.com>
2022-11-25 16:38:03 +0100

    WaterMill (need meshcollider)

--- Elementarium/Assets/Scripts/Interactable/WaterMill.cs
+++ Elementarium/Assets/Scripts/Interactable/WaterMill.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaterMill : MonoBehaviour
+public class WaterMill : Interactible
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Vector3 angularAcceleration;
+     
+    protected override void Collide(Element e)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        rb.angularVelocity += angularAcceleration;
     }
 }
