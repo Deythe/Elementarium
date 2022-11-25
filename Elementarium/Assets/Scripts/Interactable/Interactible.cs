e5d8ab683686956a513936b23005377ad6f930d1
GANDEGA <issagndega94@gmail.com>
2022-11-25 13:24:28 +0100

    particle collisions

---
+++ Elementarium/Assets/Scripts/Interactable/Interactible.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactible: MonoBehaviour
+{
+    private Element _collidedParticle;
+    [SerializeField] private ElementData.ID neededID;
+    
+    private void OnParticleCollision(GameObject other)
+    {
+        _collidedParticle = other.GetComponent<Element>();
+        
+        if (CheckCollidedElement())
+        {
+            Collide(_collidedParticle);
+        }
+        else
+        {
+            _collidedParticle = null;
+        }
+    }
+
+    private bool CheckCollidedElement()
+    {
+        if (_collidedParticle != null)
+        {
+            return (_collidedParticle.GetID() == neededID) ;
+        }
+
+        return false;
+    }
+
+    protected abstract void Collide(Element e);
+}
