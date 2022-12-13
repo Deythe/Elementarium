31a1d6864025f0d9322ac639d995f6eda200f026
Navoy <60690654+ythieme@users.noreply.github.com>
2022-12-13 21:33:59 +0100

    Intégration pipe / pressure plate et porte anim puzzle 3 à 8

--- Elementarium/Assets/Scripts/DraftYTH/DoorAnim.cs
+++ Elementarium/Assets/Scripts/DraftYTH/DoorAnim.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class DoorAnim : MonoBehaviour
 {
     private Animation doorAnim;
+    [SerializeField] bool open;
     // Start is called before the first frame update
     void Start()
     {
         doorAnim = GetComponent<Animation>();
+        if (open)
+        {
+            doorAnim.Play("DoorTest");
+        }
+        else
+        {
+            doorAnim.Play("DoorTestClose");
+        }
     }
 
     public void Open()
