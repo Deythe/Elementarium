df2403efccf5d69967622ec64264eb0a21ea9b5f
Deythe <deythe123@gmail.com>
2023-01-08 23:40:54 +0100

    Debut puzzle 10

--- Elementarium/Assets/Scripts/Interactable/Hammer.cs
+++ Elementarium/Assets/Scripts/Interactable/Hammer.cs
@@ -7,14 +7,14 @@ public class Hammer : Interactible
 {
     [SerializeField] private Rigidbody hammer_rb, pales_rb;
     [SerializeField] private float upAccelerationForce;
-    [SerializeField] private Vector3 angularAcceleration;
+    [SerializeField] private Vector3 angularAcceleration, vectorForceEnclume;
      
     protected override void Collide(Transform e)
     {
         if (e.GetComponentInParent<Element>().GetID() == ElementData.ID.AIR)
         {
             pales_rb.angularVelocity += angularAcceleration;
-            hammer_rb.AddForce(Vector3.up * upAccelerationForce, ForceMode.Acceleration);
+            hammer_rb.AddForce(vectorForceEnclume * upAccelerationForce, ForceMode.Acceleration);
         }
     }
 }
