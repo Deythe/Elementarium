c3674a3ea5214c09aa33bc7a4d195f02aee1b929
Xavier L <xavier.lezzoche@gmail.com>
2023-01-09 15:08:33 +0100

    Torch FinalState

--- Elementarium/Assets/Scripts/Interactable/Torch.cs
+++ Elementarium/Assets/Scripts/Interactable/Torch.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Torch : Brasero, ICompleted
 {
+    [SerializeField] private bool finalState;
+
     [ContextMenu("Toggle Torch")]
     public void ToggleFire() 
     {
@@ -24,6 +26,6 @@ public class Torch : Brasero, ICompleted
 
     public bool getCompletedCondition()
     {
-        return onFire;
+        return onFire && finalState;
     }
 }
