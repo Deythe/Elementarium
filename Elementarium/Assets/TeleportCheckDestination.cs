dcc37468138de637be684a1cb3ba22a06807ebae
Deythe <deythe123@gmail.com>
2023-01-22 23:02:37 +0100

    Début Check TP State

---
+++ Elementarium/Assets/TeleportCheckDestination.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TeleportCheckDestination : MonoBehaviour
+{
+    [SerializeField] private InputActionProperty _upAction;
+    private void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
